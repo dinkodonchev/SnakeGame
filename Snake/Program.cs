6bc5254bb0ce7a29b75e96b1c0f1d476e94abc33
dinkodonchev <dinyo.donchev@abv.bg>
2016-06-08 21:14:23 +0300

    first commit

---
+++ Snake/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    struct Position
+    {
+        public int col;
+        public int row;
+
+        public Position(int row, int col)
+        {
+            this.col = col;
+            this.row = row;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte right = 0;
+            byte left = 1;
+            byte down = 2;
+            byte up = 3;
+            int lastFoodTime = 0;
+            int foodDisappearTime = 8000;
+
+            //Environment.TickCount()
+            // DateTime.Now
+
+            Position[] directions = new Position[]
+            {
+                new Position(0, 1), // right
+                new Position(0, -1), // left
+                new Position(1, 0), // down
+                new Position(-1, 0), // up
+            };
+
+            double sleepTime = 100;
+            int direction = 0;
+            Random randomNumbersGenerator = new Random(); 
+            Console.BufferHeight = Console.WindowHeight;
+            Position food = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
+                randomNumbersGenerator.Next(0, Console.WindowWidth));
+            lastFoodTime = Environment.TickCount;
+            Console.SetCursorPosition(food.col, food.row);
+            Console.Write("@");
+
+            Queue<Position> snakeElements = new Queue<Position>();
+
+            for (int i = 0; i <= 5; i++)
+            {
+                snakeElements.Enqueue(new Position(0, i));
+            }
+
+            foreach (Position position in snakeElements)
+            {
+                Console.SetCursorPosition(position.col, position.row);
+                Console.Write("*");
+            }
+
+            while(true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo userInput = Console.ReadKey();
+                    if (userInput.Key == ConsoleKey.LeftArrow)
+                    {
+                        if(direction != right) direction = left;
+                    }
+                    if (userInput.Key == ConsoleKey.RightArrow)
+                    {
+                        if(direction != left) direction = right;
+                    }
+                    if (userInput.Key == ConsoleKey.UpArrow)
+                    {
+                        if (direction != down) direction = up;
+                    }
+                    if (userInput.Key == ConsoleKey.DownArrow)
+                    {
+                        if(direction != up) direction = down;
+                    }
+                }
+                Position snakeHead = snakeElements.Last();
+                Position nextDirection = directions[direction];
+                Position snakeNewHead = new Position(snakeHead.row + nextDirection.row,
+                    snakeHead.col + nextDirection.col);
+
+                if (snakeNewHead.row < 0 ||
+                    snakeNewHead.col < 0 ||
+                    snakeNewHead.row >= Console.WindowHeight ||
+                    snakeNewHead.col >= Console.WindowWidth ||
+                    snakeElements.Contains(snakeNewHead))
+                {
+                    Console.SetCursorPosition(0, 0);
+                    Console.WriteLine("Game over!");
+                    Console.WriteLine("Your score is: {0}", (snakeElements.Count-6)*100);
+                    return;
+                }
+
+                snakeElements.Enqueue(snakeNewHead);
+                Console.SetCursorPosition(snakeNewHead.col, snakeNewHead.row);
+                Console.Write("*");
+                if (snakeNewHead.col == food.col && snakeNewHead.row == food.row)
+                {
+                    // feeding the snake
+                    do
+                    {
+                        food = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
+                            randomNumbersGenerator.Next(0, Console.WindowWidth));
+                    } 
+                    while(snakeElements.Contains(food));
+                    lastFoodTime = Environment.TickCount;
+                    Console.SetCursorPosition(food.col, food.row);
+                    Console.Write("@");
+                    sleepTime--;
+
+                }
+                else
+                {
+                    // moving
+                    Position last = snakeElements.Dequeue();
+                    Console.SetCursorPosition(last.col, last.row);
+                    Console.Write(" ");
+                }
+
+                if (Environment.TickCount - lastFoodTime >= foodDisappearTime)
+                {
+                    Console.SetCursorPosition(food.col, food.row);
+                    Console.Write(" ");
+
+                    do
+                    {
+                        food = new Position(randomNumbersGenerator.Next(0, Console.WindowHeight),
+                            randomNumbersGenerator.Next(0, Console.WindowWidth));
+                    } 
+                    while(snakeElements.Contains(food));
+                    lastFoodTime = Environment.TickCount;
+                }
+
+                Console.SetCursorPosition(food.col, food.row);
+                Console.Write("@");
+                
+                sleepTime -= 0.1;
+
+                Thread.Sleep((int)sleepTime);
+            }
+        }
+    }
+}
